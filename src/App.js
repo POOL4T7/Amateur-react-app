4eb6764e23967a21f3ca01d73e17fdc0710bdd4c
Gulshan gupta <abhinavg90834@gmail.com>
2022-01-15 13:39:21 +0530

    google analytics added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Explore from "./pages/Explore";
+import reactGa from "react-ga"
 
 function App() {
+  useEffect(() => {
+    reactGa.initialize('G-6KG93CM4C7');
+    // to report page view
+    reactGa.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <Router>
       <Header />
